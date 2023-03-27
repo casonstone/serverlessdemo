29932ce81a74514a1067fe092ca3371b55f09d51
casonstone <cason.g.stone@vanderbilt.edu>
2023-03-27 10:45:34 -0500

    reput fields

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_5CeylRVZp', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '71h7oe85ptegfj88l75su8eod0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://qlijskfloj.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
